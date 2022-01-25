4bd6005859e1a0dfacdc596e5157e41566a7fa80
Lutyyyyy <98218365+Lutyyyyy@users.noreply.github.com>
2022-01-25 19:43:53 +0300

    rabotaet reverse + const chari vezde

--- onegin.cpp
+++ onegin.cpp
@@ -5,7 +5,7 @@
 
 
 const int String_size          = 1000;
-const int Number_of_strings    = 100;
+const int Number_of_strings    = 5000;
 const int Speaker_name_len     = 3;
 const char* bad_symbols_string = " ,.\t!?:;";
 
@@ -15,19 +15,18 @@ const char* bad_symbols_string = " ,.\t!?:;";
 char* Get_string (char* str, size_t String_size, FILE* file);
 
 
-void start_bubblesort(char* array[], int len);
+void start_bubblesort(const char* array[], int len);
 
 int  skip_bad_start (const char* str);
 int  start_ignoring_cmp (const char* str1, const char* str2);
 
 
-void end_bubblesort(char* array[], int len);
+void end_bubblesort(const char* array[], int len);
 
 int  skip_bad_end (const char* str, int len);
 int  reverse_stricmp (const char* str1, int len_1,
                       const char* str2, int len_2);
-int  end_ignoring_cmp (char* str1, int len1,
-                       char* str2, int len2);
+int  end_ignoring_cmp (const char* str1, const char* str2);
 
 
 void Space_cleaning (char* str);
@@ -35,7 +34,7 @@ void End_of_line_cleaning (char* str);
 void Speaker_cleaning (char* str);
 
 
-void swap (char* array[], int i, int j);
+void swap (const char* array[], int i, int j);
 
 //-----------------------------------------------------------------------------
 
@@ -46,8 +45,8 @@ int main(int argc, char* argv[])
 
     FILE* Onegin = fopen ((argc == 1) ? "hamlet.txt" : argv[1], "r");
 
-    char strings[Number_of_strings][String_size] = {};
-    char* string_pointers[Number_of_strings] = {};
+    static char strings[Number_of_strings][String_size] = {};
+    const char* string_pointers[Number_of_strings] = {};
 
     int nStrings = 0;
     for ( ; nStrings < Number_of_strings; nStrings++)
@@ -55,31 +54,20 @@ int main(int argc, char* argv[])
         char* line = Get_string (strings[nStrings], String_size - 1, Onegin);
         if (line == NULL)
             break;
-
-        //Space_cleaning (line);
-        //End_of_line_cleaning (line);
-        //Speaker_cleaning (line);
         }
 
     for (int i = 0; i < nStrings; i++)
         string_pointers[i] = (char*) &strings[i];
 
     for (int i = 0; i < nStrings; i++)
-        {
         printf("String #[%d] = (%s)\n", i, string_pointers[i]);
-        }
 
     printf("\n\n\n");
 
     end_bubblesort (string_pointers, nStrings);
 
     for (int i = 0; i < nStrings; i++)
-        {
         printf("String #[%d] = (%s)\n", i, string_pointers[i]);
-        }
-
-
-
 
     fclose (Onegin);
 
@@ -107,7 +95,7 @@ char* Get_string (char* str, size_t size, FILE* file)
 
 //-----------------------------------------------------------------------------
 
-void start_bubblesort (char* array[], int len)
+void start_bubblesort (const char* array[], int len)
     {
     int swaps = 1;
     while (swaps != 0)
@@ -154,7 +142,7 @@ int skip_bad_start (const char* str)
 
 //-----------------------------------------------------------------------------
 
-void end_bubblesort (char* array[], int len)
+void end_bubblesort (const char* array[], int len)
     {
     int swaps = 1;
     while (swaps != 0)
@@ -166,8 +154,8 @@ void end_bubblesort (char* array[], int len)
             assert (0 <= i && i < len - 1);
             assert (0 <= i + 1 && i + 1 < len);
 
-            if (end_ignoring_cmp (array[i],     strlen (array[i]),
-                                  array[i + 1], strlen (array[i + 1])) > 0)
+            int res = end_ignoring_cmp (array[i], array[i + 1]);
+            if (res > 0)
                 {
                 swap (array, i, i + 1);
                 swaps++;
@@ -178,20 +166,23 @@ void end_bubblesort (char* array[], int len)
 
 //-----------------------------------------------------------------------------
 
-int end_ignoring_cmp (char* str1, int len1,
-                      char* str2, int len2)
+int end_ignoring_cmp (const char* str1, const char* str2)
     {
-    return (reverse_stricmp (str1, len1 - skip_bad_end (str1, len1),
-                             str2, len2 - skip_bad_end (str2, len2)));
+    return (reverse_stricmp (str1, skip_bad_end (str1, strlen(str1)),
+                                 str2, skip_bad_end (str2, strlen(str2))));
     }
 
 //-----------------------------------------------------------------------------
 
 int skip_bad_end (const char* str, int len)
     {
-    int symbols_counter = len - 1;
+    if (len == 0)
+        return 0;
+
+    int symbols_counter = len;
     while (strchr(bad_symbols_string, str[symbols_counter]))
         symbols_counter--;
+
     return symbols_counter;
     }
 
@@ -203,9 +194,8 @@ int reverse_stricmp (const char* str1, int len_1,
     assert (str1 != NULL);
     assert (str2 != NULL);
 
-    int i_1 = len_1 - 1, i_2 = len_2 - 1;
-
-    while (i_1 >= 0 && i_2 >= 0 && tolower(str1[i_1]) == tolower(str2[i_2]));
+    int i_1 = len_1, i_2 = len_2;
+    while (i_1 >= 0 && i_2 >= 0 && tolower(str1[i_1]) == tolower(str2[i_2]))
         {
         if (i_1 == 0 || i_2 == 0)
             return (tolower(str1[i_1]) - tolower(str2[i_2]));
@@ -264,14 +254,16 @@ void Speaker_cleaning (char* str)
 
 //-----------------------------------------------------------------------------
 
-void swap (char* array[], int i, int j)
+void swap (const char* array[], int i, int j)
     {
-    char* temp = array[i];
+    const char* temp = array[i];
     array[i] = array[j];
     array[j] = temp;
     }
 
 //-----------------------------------------------------------------------------
+
+
 
 
 
